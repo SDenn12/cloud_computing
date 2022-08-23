8f3e371241fcf780b98741ae7ef69d6196bb9d0c
SDenn12 <110126036+SDenn12@users.noreply.github.com>
2022-08-23 10:23:53 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -17,18 +17,18 @@ sudo apt-get install npm
 sudo npm install pm2 -g
 
 # automate reverse proxy
-sudo cp -f app/rev_prox_file /etc/nginx/sites-available/default
+# sudo cp -f app/rev_prox_file /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 
-# declares env variable
-echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
+# # declares env variable
+# echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
 
-# updates the seed
-cd app/app
-npm install
-cd seeds
-sudo node seed.js
+# # updates the seed
+# cd app/app
+# npm install
+# cd seeds
+# sudo node seed.js
 
-# system update
-sudo apt-get update
-sudo apt-get upgrade -y
+# # system update
+# sudo apt-get update
+# sudo apt-get upgrade -y
